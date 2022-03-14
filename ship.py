bd103315de8e7f07373268facee63d4055f5fb5f
Jeetpatel6601 <101332374+Jeetpatel6601@users.noreply.github.com>
2022-03-14 15:53:03 +0530

    Update ship.py

--- ship.py
+++ ship.py
@@ -7,7 +7,7 @@ class Ship(Sprite):
 		self.screen = ai_game.screen
 		self.settings = ai_game.settings
 		self.screen_rect = ai_game.screen.get_rect()
-		self.image = pygame.image.load('images/t_ship.bmp')
+		self.image = pygame.image.load('images/ship.bmp')
 		self.rect = self.image.get_rect()
 		self.rect.midbottom = self.screen_rect.midbottom
 		self.x = float(self.rect.x)
